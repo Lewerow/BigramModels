fcee104b984602df8f237f71dfbacad25bcb0e90
Lewerow <lewerow@wp.pl>
2014-11-24 20:52:16 +0100

    Refa

--- extract.py
+++ extract.py
@@ -2,29 +2,24 @@ import xml.etree.ElementTree as etree
 import sys
 import getopt
 
-def print_orths(tokens):
-  for tok in tokens:
-    print(tok.find('orth').text)
+def get_orths(tokens):
+  return [tok.find('orth').text for tok in tokens]
 
-def print_lemmas(tokens):
-  for tok in tokens:
-    print(tok.find('lex').find('base').text)
+def get_lemmas(tokens):
+  return [tok.find('lex').find('base').text for tok in tokens]
 
-def print_grammar(tokens):
-  for tok in tokens:
-    print(tok.find('lex').find('ctag').text)
+def get_grammar(tokens):
+  return [tok.find('lex').find('ctag').text for tok in tokens]
 
 
+getters = {'orth' : get_orths, 'lex' : get_lemmas, 'ctag' : get_grammar}
 def execute():
+  global getters
   parser = etree.parse(sys.argv[1])
   tokens = parser.findall('//tok')
-
-  if sys.argv[2] == 'orth':
-    print_orths(tokens)
-  elif sys.argv[2] == 'lex':
-    print_lemmas(tokens)
-  elif sys.argv[2] == 'ctag':
-    print_grammar(tokens)
+  
+  values = getters[sys.argv[2]](tokens)
+  print(' '.join(values))
 
 if __name__ == "__main__":
   execute()
