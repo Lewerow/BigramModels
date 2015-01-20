d162c9afaa6d01009a9b8cbbf5ee5c1248a19b6b
usernamealreadynottaken <maciej.laszczyk@gmail.com>
2015-01-20 15:22:19 +0100

    Evalutation script - eval.py

--- eval.py
+++ eval.py
@@ -1,29 +1,45 @@
-import subprocess
+from sys import stdin
 import sys
-import re
+import subprocess
+import extract
 
+# params
+# lex / ctag
+# model
+
+temp_dir = "temp/"
 
 def execute():
-    p = subprocess.Popen(["ngramprint", "--ARPA", sys.argv[1]],
-                         stdout=subprocess.PIPE,
-                         stderr=subprocess.PIPE)
-    out, err = p.communicate()
+    global temp_dir;
+    subprocess.call(["mkdir", "-p", temp_dir])
+    line = stdin.readline()
+    while (line != "\n"):
+        plain = open(temp_dir + "plain.txt", 'w+')
+	plain.write(line + '\n');
+        plain.close()
+        subprocess.call(["wcrft-app", "nkjp_e2",
+                     "-i", "txt", temp_dir + "plain.txt", 
+		     "-O", temp_dir + "tagged.xml"])
+	extracted = extract.execute(temp_dir + "tagged.xml", sys.argv[1])
 
-    lines = out.splitlines()
-    numunigrams = int(re.findall(r"\d+", lines[2])[-1])
-    numbigrams = int(re.findall(r"\d+", lines[3])[-1])
+        tagged = open(temp_dir + "extracted.txt", "w")
+        tagged.write(extracted + '\n')
+        tagged.close()
 
-    # I assume same format for every file
-    distance = 2
-    startunigrams = 6
-    startbigrams = startunigrams + numunigrams + distance
+	symbols = open(temp_dir + "symbols.syms", "w")
+        subprocess.call(["ngramsymbols", temp_dir + "extracted.txt"], stdout=symbols)
+        symbols.close()
 
-    unigrams = []
-    for n in range(startunigrams, startunigrams + numunigrams):
-        unigrams.append(lines[n])
-    bigrams = []
-    for n in range(startbigrams, startbigrams + numbigrams):
-        bigrams.append(lines[n])
+	far = open(temp_dir + "far.far", "w")
+        subprocess.call(["farcompilestrings",
+                     "-symbols=" + temp_dir + "symbols.syms",
+                     "-keep_symbols=1", temp_dir + "extracted.txt"], stdout=far)
+        far.close()
+
+	subprocess.call(["ngramperplexity", sys.argv[2], temp_dir + "far.far"])
+
+	line = stdin.readline()
+	
 
 if __name__ == "__main__":
     execute()
